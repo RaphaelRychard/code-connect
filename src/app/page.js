463120e493720142820e1b21b9fcd1478cfecbe9
Raphael Rychard <raph.rych@gmail.br>
2024-04-21 15:38:56 -0400

    add pagination

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,34 @@
+import logger from '@/logger';
 import { CardPost } from '@/components/CardPost';
+
 import './globals.css'
+import Link from 'next/link';
 
-
-// const post = {
-//   "id": 1,
-//   "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-//   "title": "Introdução ao React",
-//   "slug": "introducao-ao-react",
-//   "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-//   "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-//   "author": {
-//     "id": 101,
-//     "name": "Ana Beatriz",
-//     "username": "anabeatriz_dev",
-//     "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
-//   }
-// }
-
-async function getAllPosts() {
-  const response = await fetch('http://localhost:3042/posts')
+async function getAllPosts(page) {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
   if (!response.ok) {
-    console.log('Ops, alguma coisa correu mal');
+    logger.error('Ops, alguma coisa correu mal');
+    return []
   }
+  logger.info('Posts obtidos com sucesso')
   return response.json()
 }
 
-export default async function Home() {
-  const posts = await getAllPosts()
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1
+  const { data: posts, prev, next } = await getAllPosts(currentPage)
 
   return (
-    <main className="main-container">
-      {posts.map(post => <CardPost post={post} />)}
-    </main>
+    <>
+      <main>
+        <div className="main-container">
+          {posts.map(post => <CardPost key={post.id} post={post} />)}
+        </div>
+        <div className='div-links'>
+          {prev && <Link className='links_prev' href={`/?page=${prev}`}>Pagína Enterior</Link>}
+          {next && <Link className='links_next' href={`/?page=${next}`}>Proxima Pagína</Link>}
+        </div>
+      </main>
+    </>
   );
 }
